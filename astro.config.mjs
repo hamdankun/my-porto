43685c785400f6ea8e221ef70b862c37b9b5d212
Hamdan Hanafi <hamdanhanafi90@gmail.com>
2025-07-20 16:03:55 +0700

    fix: load assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hamdankun.github.io/my-porto',
-  base: '/my-porto',
+  base: '/my-porto/',
   outDir: 'docs',
   integrations: [react()],
   vite: {
